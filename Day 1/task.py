22a0305ea2a644d5701767cc2a7ac5b540f15f8e
michael <jipmichael1@gmail.com>
2026-06-29 16:15:23 +0700

    Chore: 1 Day

---
+++ Day 1/task.py
@@ -0,0 +1,24 @@
+# print("Hello World")
+
+# print("Hello" + " " + "Bob")
+
+# print("Hello this is bob\nfrom germany")
+
+# print("Hello " + input("What is your name?") + "!")
+
+# name = input("What is your name? ")
+# print(name)
+
+# username = input("What is your username? ")
+# length = len(username)
+
+# print(username)
+# print(length)
+
+superhero = input("What is your favorite superhero? ")
+pet = input("What is your pet name? ")
+
+print("Welcome to pet hero generator")
+print(superhero)
+print(pet)
+print("Your superhero pet is:", superhero, pet)
