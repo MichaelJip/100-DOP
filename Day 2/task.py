7652d0973dfff8c763abb7d830c4b0ea55512291
michael <jipmichael1@gmail.com>
2026-06-30 16:19:55 +0700

    Chore: Update Day 2

--- Day 2/task.py
+++ Day 2/task.py
@@ -3,11 +3,11 @@ print("Hello"[4])
 
 print("Welcome to tip calculator!")
 
-bill = input("What was the total bill? $")
-tip = input("How much tip would you like to give? 10, 12, or 15 ")
-people = input("How many people to split the bill? ")
+bill = float(input("What was the total bill? $"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15 "))
+people = int(input("How many people to split the bill? "))
 
-bill_and_tip = float(bill) + (int(tip) / 100) 
-calculate_total = float(bill_and_tip) / int(people)
+bill_and_tip = bill + (tip / 100) 
+calculate_total = float(bill_and_tip) / people
 
 print(f"Each person should pay: ${calculate_total}")
